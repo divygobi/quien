caf182c7e2eb52cb6b174bfa02b4b51c90170ed8
Divy Gobiraj <divygobiraj@mac.lan>
2026-02-24 00:38:57 -0800

    Self reproducing achieved

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 int main()
 {
     char buf[2048];
-    const char *self = "#include <cstdio>\n#include <stdio.h>\n#include <string>\n\nint main()\n{\n    char buf[2048];\n    const char *self = \"?\";\n    \n    while(true) {\n        char *s = fgets(buf, sizeof(buf), stdin);\n\n        if (s == NULL) {\n          printf(\"Null\");\n          return 0;\n        }\n        \n        std::string meowArr = \"meow\";\n\n        while (*s){\n          bool found = true;\n          for (int i = 0; i < 4; i++){\n            //TODO see behavior of s[i] on random garbage values \n            found &= (s[i] && meowArr[i] == s[i]);\n          }\n          if (found){\n\n            int n = strlen(self);\n            printf(\"Made it in here, with len %i\", n);\n          \n            //Loop through self to replace anchor point with \n            for (int i = 0; i < n; i++){\n\n              printf(\"%i\", self[i]);\n              if (self[i] == '?'){\n                //Need to add escape logic to process string\n                for (int j = 0; j < n; j++){\n                  switch (self[j]){\n                    case '\\n': printf(\"\\\\n\"); break;\n                    case '\\\\': printf(\"\\\\\\\\\"); break;\n                    case '\\\"': printf(\"\\\\\\\"\"); break;\n                    default: printf(\"%c\", self[j]);\n                  }\n               }\n              } \n              else {\n              printf(\"%c\", self[i]);\n            }\n\n            return 0;\n           }\n          }\n        \n        }\n\n    return 0;\n}\n}\n\n";
+    const char *self = "#include <cstdio>\n#include <stdio.h>\n#include <string>\n\nint main()\n{\n    char buf[2048];\n    const char *self = \"?\";\n    \n    while(true) {\n        char *s = fgets(buf, sizeof(buf), stdin);\n\n        if (s == NULL) {\n          printf(\"Null\");\n          return 0;\n        }\n        \n        std::string meowArr = \"meow\";\n\n        while (*s){\n          bool found = true;\n          for (int i = 0; i < 4; i++){\n            //TODO see behavior of s[i] on random garbage values \n            found &= (s[i] && meowArr[i] == s[i]);\n          }\n          if (found){\n\n            int n = strlen(self);\n          \n            //Loop through self to replace anchor point with \n            for (int i = 0; i < n; i++){\n              if (self[i] == 63){\n                //Need to add escape logic to process string\n                for (int j = 0; j < n; j++){\n                  switch (self[j]){\n                    case '\\n': printf(\"\\\\n\"); break;\n                    case '\\\\': printf(\"\\\\\\\\\"); break;\n                    case '\\\"': printf(\"\\\\\\\"\"); break;\n                    default: printf(\"%c\", self[j]);\n                  }\n               }\n              } \n              else {\n                printf(\"%c\", self[i]);\n              }\n\n           }\n          }\n          s+=4;\n        }\n    return 0;\n  }\n}\n";
     
     while(true) {
         char *s = fgets(buf, sizeof(buf), stdin);
@@ -26,12 +26,10 @@ int main()
           if (found){
 
             int n = strlen(self);
-            printf("%i", n);
-            printf("\n");
           
             //Loop through self to replace anchor point with 
             for (int i = 0; i < n; i++){
-              if (self[i] == '?'){
+              if (self[i] == 63){
                 //Need to add escape logic to process string
                 for (int j = 0; j < n; j++){
                   switch (self[j]){
